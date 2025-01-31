b28457063b49f70978056831a4d4c448de433bf7
Mark Joshua Austria <austriamark.mja@gmail.com>
2025-01-31 18:02:53 +0800

    refactor ShoeCard, Footer, and Hero components for improved key handling and layout consistency

--- src/components/ShoeCard.jsx
+++ src/components/ShoeCard.jsx
@@ -7,14 +7,11 @@ const ShoeCard = ({ imgURL, changeBigShoeImg, bigShoeImg }) => {
 
   return (
     <div
-      className={`border-2 rounded-xl ${
+      className={`border-2 rounded-xl cursor-pointer max-sm:flex-1 ${
         bigShoeImg === imgURL.bigShoe
           ? "border-coral-red"
           : "border-transparent"
-      }
-    `}
-      cursor-pointer
-      max-sm:flex-1
+      }`}
       onClick={handleClick}
     >
       <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -16,7 +16,10 @@ const Footer = () => {
           </p>
           <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
-              <div className="flex justify-center items-center w-12 h-12 bg-white rounded-full">
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                key={icon.alt}
+              >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
             ))}
@@ -24,7 +27,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-1 justify-between lg:gap-10 gap-20 flex-wrap">
           {footerLinks.map((section) => (
-            <div key={section}>
+            <div key={section.title}>
               <h4 className="text-white font-montserrat text-2xl leading-normal font-medium mb-6 ">
                 {section.title}
               </h4>
